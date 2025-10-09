61573714d5e3418f28601e0a9343a44746aaebf2
Vinay Mohan <vinaymohanvinod@gmail.com>
2025-10-09 09:16:39 +0530

    google signin added to signup

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,13 +1,18 @@
 "use server";
 
 import { signUp } from "@/actions/auth.actions";
+import GoogleLoginButton from "@/components/GoogleAuthButton";
 
 export default async function SignupPage() {
   return (
+    <div>
+
     <form action={signUp} className="flex flex-col gap-2">
       <input name="email" type="email" placeholder="Email" required />
       <input name="password" type="password" placeholder="Password" required />
       <button type="submit">Sign Up</button>
     </form>
+    <GoogleLoginButton />
+    </div>
   );
 }
